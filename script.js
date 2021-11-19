d237fef1cd3ae409de5b02b0a99c95e45cd392af
Taegan Loyzelle <tloyzelle@gmail.com>
2021-11-19 18:03:45 -0500

    Final updates

--- script.js
+++ script.js
@@ -36,3 +36,22 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+
+function validateForm()
+{
+/* Validating name field */
+var x=document.forms["myForm"]["name"].value;
+if (x==null || x=="")
+ {
+ alert("Name must be filled out");
+ return false;
+ }
+/* Validating email field */
+var x=document.forms["myForm"]["email"].value;
+var atpos=x.indexOf("@");
+var dotpos=x.lastIndexOf(".");
+if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+ {
+ alert("Not a valid e-mail address");
+ return false;
+ }};
